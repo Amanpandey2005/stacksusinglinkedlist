9686ec2771767aeb7590dc57b7a7fa26a9911b59
Aman Pandey <147979955+Amanpandey2005@users.noreply.github.com>
2024-01-08 20:33:14 +0530

    Add files via upload

--- stacksusinglinkedlist.c
+++ stacksusinglinkedlist.c
@@ -4,12 +4,14 @@ struct node{
     int data ;
     struct node* next ;
 };
+struct node* top = NULL;
 void linkedlisttraversal(struct node*ptr){
     while(ptr!=NULL){
-        printf("element :- %d ", ptr-> data);
+        printf("element :- %d\n ", ptr-> data);
         ptr = ptr->next; 
     }
 }
+
 int isempty(struct node* top){
     if(top == NULL){
         return 1;
@@ -17,6 +19,7 @@ int isempty(struct node* top){
     else
     return 0;
 }
+
 int isfull(struct node*top){
     struct node*p = (struct node*)malloc(sizeof(struct node));
     if(p == NULL)
@@ -27,6 +30,7 @@ int isfull(struct node*top){
     return 0;
     }
 }
+
 struct node* pushstackinlinkedlist(struct node* *top, int value){
     if(isfull(*top)){
     printf("stack overflow");
@@ -40,6 +44,7 @@ struct node* pushstackinlinkedlist(struct node* *top, int value){
     }
 
 }
+
 int popstackinlinkedlist(struct node **top){
     if(isempty(*top)){
         printf("stack underflow");
@@ -48,17 +53,38 @@ int popstackinlinkedlist(struct node **top){
         struct node*n = *top;
     *top = (*top)-> next;
     int x = n-> data;
+    free(n);
+    return x;
     }
 }
+
+int peek(int pos){
+    struct node* ptr= top;
+    for ( int i = 0; i < pos-1 && ptr!= NULL; i++)
+    {
+        ptr = ptr-> next;
+    }
+    if(ptr!= NULL){
+        return ptr-> data;
+    }
+    else{
+        return -1;
+    }
+}
+
 int main(){
- struct node*top = NULL;
     pushstackinlinkedlist(&top,45);
     pushstackinlinkedlist(&top,90);
     pushstackinlinkedlist(&top,56);
     pushstackinlinkedlist(&top,87);
     pushstackinlinkedlist(&top,7);
      linkedlisttraversal(top);
-     printf("\n");
+     for (int i = 1; i <= 5; i++)
+     {
+        printf("value at position %d is %d\n ", i, peek(i));
+     }
+     
+     printf("values after popping\n");
     popstackinlinkedlist(&top);
     linkedlisttraversal(top);
 }
